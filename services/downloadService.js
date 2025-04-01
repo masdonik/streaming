59ea6eaf53a70afeb47a93d6fa6000d0d48f8cc7
masdonik <masdonik@github.com>
2025-04-01 06:57:22 +0000

    fix: use absolute python path to ensure correct gdown execution

--- services/downloadService.js
+++ services/downloadService.js
@@ -50,7 +50,7 @@ class DownloadService {
             }
 
             // Spawn gdown process tanpa -O untuk menggunakan nama file asli
-            const gdownProcess = spawn('python3', ['-m', 'gdown', 
+            const gdownProcess = spawn('/home/codespace/.python/current/bin/python3', ['-m', 'gdown', 
                 `https://drive.google.com/uc?id=${fileId}`
             ], {
                 cwd: this.downloadPath // Set working directory ke folder video
